519a8602312e287847ed34964eb3bee8adfc4aa8
Ildar <ildarthegreat@gmail.com>
2022-09-06 21:30:11 +0400

    init commit

---
+++ src/main/java/ru/redmadrobot/red_mad_robot_test/RedMadRobotTestApplication.java
@@ -0,0 +1,13 @@
+package ru.redmadrobot.red_mad_robot_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedMadRobotTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RedMadRobotTestApplication.class, args);
+    }
+
+}
